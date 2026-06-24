09814cabcb146943244a95e3a7b3d83d425a7d1f
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2026-06-24 19:55:14 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { LineChart } from "layerchart";
+    import { createDateSeries } from "$lib/data";
+
+    const data = createDateSeries({
+        count: 30,
+        min: 50,
+        max: 100,
+        value: "integer",
+    });
+</script>
+
+<h1>Welcome to My Project</h1>
+
+<LineChart {data} x="date" y="value" height={300} />
